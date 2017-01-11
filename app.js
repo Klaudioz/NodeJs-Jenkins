cbcd412d1cf9b1b75385d08092d5ab2b00312080
Klaudioz <klaudioz@gmail.com>
2017-01-11 13:34:26 -0800

    Jenkins crashed last time

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 app.get('/',function(req,res) {
 	res.send("Continuous Integration and deployment with Jenkins and Node.js");
 	res.send("1. Testing Github and Jenkins talking to each other");
-	res.send("2. Installing npm in DigitalOcean droplet - console output Jenkins: npm: not found");
+	res.send("2. Installing npm in DigitalOcean droplet - console output Jenkins: npm: not found ");
 });
 
 app.listen(4000);
