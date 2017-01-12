b991354a835943374deaca49c291f372b779260c
root <root@ubuntu-512mb-sfo2-01-M2.localdomain>
2017-01-12 01:13:21 +0000

    'using development machine'

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ var app = express();
 
 app.get('/',function(req,res) {
 	res.send("Continuous Integration and deployment with Jenkins and Node.js");
-	res.send("1. Testing Github and Jenkins talking to each other");
-	res.send("2. Installing npm in DigitalOcean droplet - console output Jenkins: npm: not found ");
+	res.end("Proyect in development droplet");
 });
 
 app.listen(4000);
