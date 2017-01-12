7246cce6d933b89049dd64bc370eab9deefbf8d4
Klaudioz <klaudioz@gmail.com>
2017-01-12 02:00:26 +0000

    adding credentials

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res) {
-	res.send("Continuous Integration and deployment with Jenkins and Node.js - Test Final 0.5");
+	res.send("Continuous Integration and deployment with Jenkins and Node.js - Test Final 0.6");
 	res.end("Proyect in development droplet");
 });
 
